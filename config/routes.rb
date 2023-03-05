8d65091aef5bf521e379e960e4539b2d37885ea6
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-05 11:11:29 +0100

    add the correct routes with correct users association

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :owners
-  devise_for :customers
+  devise_for :owners, path: 'owners', controllers: {
+    sessions: 'owners/sessions', registrations: 'owners/registrations', passwords: 'owners/passwords'
+  }
+  devise_for :customers, path: 'customers', controllers: {
+    sessions: 'owners/sessions', registrations: 'owners/registrations', passwords: 'owners/passwords'
+  }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
