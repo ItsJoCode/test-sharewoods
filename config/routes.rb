9d16cbfc5a4e80fc363d54a1dcc2d2786d6fb6dc
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-05 12:09:35 +0100

    add authenticate Customer and Owner with both users path associated when log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     sessions: 'owners/sessions', registrations: 'owners/registrations', passwords: 'owners/passwords'
   }
   devise_for :customers, path: 'customers', controllers: {
-    sessions: 'owners/sessions', registrations: 'owners/registrations', passwords: 'owners/passwords'
+    sessions: 'customers/sessions', registrations: 'customers/registrations', passwords: 'customers/passwords'
   }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
